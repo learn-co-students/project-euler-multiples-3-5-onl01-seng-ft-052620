b4e3e0214c59388e2ee1dcc9923d8d654636cbf9
dwandrew <danwandrew@gmail.com>
2020-11-12 15:41:02 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+class Multiples
+    attr_reader :limit, :multiples
+
+    def initialize(limit)
+      @limit = limit - 1
+      @multiples = collect_multiples
+    end
+
+    def collect_multiples
+        i=1
+        multiples = []
+            while i <= limit do
+                if (i%3 == 0) || (i%5 == 0)
+                    multiples.push(i)
+                end
+                i+=1
+            end
+        return multiples
+        end
+        
+        def sum_multiples
+        
+        array = collect_multiples
+        return array.reduce(0) { |sum, num| sum + num }
+        
+        end
+end
