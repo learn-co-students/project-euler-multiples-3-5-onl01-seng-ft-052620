25f77a3d9131702953d630b3272cf0040ae1b191
sean-slaughter <sthompson2822@gmail.com>
2020-11-29 19:01:22 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        (1..(self.limit - 1)).select do |i|
+            i % 3 == 0 || i % 5 == 0
+        end
+    end
+
+    def sum_multiples
+        multiples = self.collect_multiples
+        output = 0
+        multiples.each{|i| output += i}
+        return output
+    end
+
+end
