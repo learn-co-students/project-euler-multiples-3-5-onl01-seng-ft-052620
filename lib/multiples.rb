b6b9a16d764f702d17b60f1afed5aa5a68a3f4a4
aaronfeingold <ajfeingold88@gmail.com>
2021-02-14 22:57:37 -0600

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,23 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  a = Array.new
+
+    for x in 1...limit do
+      if x % 3 == 0 || x % 5 == 0
+        a.push(x)
+      end
+    end
+
+  return a
+
+end
+
+def sum_multiples(limit)
+
+  sum = 0
+
+  collect_multiples(limit).each { |num| sum += num}
+
+  return sum
+
+end 
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,26 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  attr_reader :limit, :multiples
+
+  def initialize(limit)
+    @limit = limit
+    @multiples = collect_multiples
+  end
+
+
+
+  def collect_multiples
+
+    (1...limit).to_a.reject { |x| x unless x % 3 == 0 || x % 5 == 0 }
+  
+  end
+  
+  def sum_multiples
+    
+    multiples.reduce(:+)
+  
+  end
+
+
+end 
