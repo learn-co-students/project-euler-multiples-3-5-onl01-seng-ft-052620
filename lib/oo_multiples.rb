19d7044396a7dedb9c9acdd610b7b8f5c6d8ad61
aaronfeingold <ajfeingold88@gmail.com>
2021-02-15 14:07:30 -0600

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -2,12 +2,12 @@
 class Multiples
 
   def initialize(limit)
-    @limit = limit
+    @limit = limit - 1
     @multiples = collect_multiples
   end
 
   def collect_multiples
-    (1...@limit).to_a.reject { |x| x unless x % 3 == 0 || x % 5 == 0 } 
+    (1..@limit).to_a.reject { |x| x unless x % 3 == 0 || x % 5 == 0 } 
   end
   
   def sum_multiples  
